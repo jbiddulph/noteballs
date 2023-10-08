1bd9db3bf2a2ade69a3ec3d24f16d07891cb79b2
jbiddulph <john.mbiddulph@gmail.com>
2023-10-08 22:12:54 +0100

    styling navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 
 <template>
-  <RouterLink to="/">Notes</RouterLink> | 
-  <RouterLink to="/stats">Stats</RouterLink>
-  <RouterView />
+  <NavBar />
+  <div class="container is-max-desktop px-2 py-4">
+    <RouterView />
+  </div>
 </template>
+
+<script setup>
+/* Imports */
+import NavBar from '@/components/layout/NavBar.vue'
+</script>
+
+<style>
+  @import 'bulma/css/bulma.min.css';
+</style>
---
+++ src/components/layout/NavBar.vue
@@ -0,0 +1,71 @@
+<template>
+  <nav
+    class="navbar is-success"
+    aria-label="main navigation"
+    role="navigation"
+  >
+    <div class="container is-max-desktop px-2">
+      <div class="navbar-brand">
+        <div class="navbar-item is-size-4 is-family-monospace">
+          NOTEBALLS
+        </div>
+
+        <a
+          @click.prevent="showMobileNav = !showMobileNav"
+          class="navbar-burger"
+          :class="{ 'is-active' : showMobileNav }"
+          aria-expanded="false"
+          aria-label="menu"
+          data-target="navbarBasicExample"
+          role="button"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div
+        id="navbarBasicExample"
+        class="navbar-menu"
+        :class="{ 'is-active' : showMobileNav }"
+      >
+        
+        <div class="navbar-end">
+          <RouterLink
+            class="navbar-item"
+            active-class="is-active"
+            to="/"
+          >
+            Notes
+          </RouterLink>
+          <RouterLink
+            class="navbar-item"
+            active-class="is-active"
+            to="/stats"
+          >
+            Stats
+          </RouterLink>
+          
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+/* Imports */
+import { ref } from 'vue'
+/* Mobile Nav */
+const showMobileNav = ref(false)
+</script>
+
+<style scoped>
+@media (max-width: 1023px) {
+  .navbar-menu {
+    position: absolute;
+    left: 0;
+    width: 100%; 
+  }  
+}
+</style>
